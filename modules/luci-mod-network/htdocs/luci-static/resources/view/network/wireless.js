29269a26e01512c284fb12bcd9363e9eea0660ca
James Haggerty <james.haggerty@morsemicro.com>
2024-10-31 23:30:51 +0000

    luci-mod-network: (APP-3504, PR #130) restrict beacon interval to 10000
    Apparently, though hostapd allows this, cfg80211 was modified
    such that >10000 beacon intervals aren't accepted.
    
    
    Approved-by: Lyall Beveridge
    Approved-by: Evan Benn

--- modules/luci-mod-network/htdocs/luci-static/resources/view/network/wireless.js
+++ modules/luci-mod-network/htdocs/luci-static/resources/view/network/wireless.js
@@ -1057,7 +1057,7 @@ return view.extend({
 					o.depends('mode', 'mesh');
 
 					o = ss.taboption('advanced', form.Value, 'beacon_int', _('Beacon Interval'));
-					o.datatype = 'range(15,65535)';
+					o.datatype = 'and(uinteger,range(15,10000))';
 					o.placeholder = 100;
 					o.rmempty = true;
 
@@ -1129,7 +1129,7 @@ return view.extend({
 					o.datatype = 'range(1,2147483647)';
 
 					o = ss.taboption('advanced', form.Value, 'beacon_int', _('Beacon Interval'));
-					o.datatype = 'range(15,65535)';
+					o.datatype = 'and(uinteger,range(15,10000))';
 					o.placeholder = 100;
 					o.rmempty = true;
 				}
