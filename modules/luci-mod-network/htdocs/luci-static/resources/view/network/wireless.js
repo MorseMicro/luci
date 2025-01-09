3eb476e50e731a8ca8ced7762477b8931839bfa7
James Haggerty <james.haggerty@morsemicro.com>
2025-01-09 01:00:41 +0000

    luci-mod-network: (APP-3946, PR #148) remove unnecessary sae_pwe setting
    The netifd-morse backend will set this to 1 automatically.
    
    Approved-by: Sophronia Koilpillai
    Approved-by: Lyall Beveridge

--- modules/luci-mod-network/htdocs/luci-static/resources/view/network/wireless.js
+++ modules/luci-mod-network/htdocs/luci-static/resources/view/network/wireless.js
@@ -1392,11 +1392,6 @@ return view.extend({
 						var e = this.section.children.filter(function (o) { return o.option == 'encryption' })[0].formvalue(section_id);
 
 						uci.set('wireless', section_id, 'encryption', e);
-						if (e == 'sae')
-							uci.set('wireless', section_id, 'sae_pwe', 1);
-						else
-							uci.unset('wireless', section_id, 'sae_pwe');
-
 					};
 
 
@@ -1588,11 +1583,6 @@ return view.extend({
 						var e = this.section.children.filter(function (o) { return o.option == 'mesh_encryption' })[0].formvalue(section_id);
 
 						uci.set('wireless', section_id, 'encryption', e);
-						if (e == 'sae')
-							uci.set('wireless', section_id, 'sae_pwe', 1);
-						else
-							uci.unset('wireless', section_id, 'sae_pwe');
-
 					};
 
 					o.value('sae', '%s (%s)'.format('WPA3-SAE', _('strong security')));
