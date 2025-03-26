3d0110ca1df75af24b8eca17bc19884d5c1b85ff
James Haggerty <james.haggerty@morsemicro.com>
2025-03-26 02:02:02 +0000

    luci-mod-network/luci-mod-dashboard: (APP-4289, PR #164) Add missing freqUnit calls
    Every time we use getFrequency, we need to use getFrequencyUnit
    to show MHz or GHz. Chase the remaining instances of this.
    
    Approved-by: Sophronia Koilpillai
    Approved-by: Prabha Murugesan

--- modules/luci-mod-dashboard/htdocs/luci-static/resources/view/dashboard/include/30_wifi.js
+++ modules/luci-mod-dashboard/htdocs/luci-static/resources/view/dashboard/include/30_wifi.js
@@ -141,6 +141,7 @@ return baseclass.extend({
 					 is_assoc = (net.getBSSID() != '00:00:00:00:00:00' && net.getChannel() && !net.isDisabled()),
 					 chan = net.getChannel(),
 					 freq = net.getFrequency(),
+					 frequnit = net.getFrequencyUnit(),
 					 rate = net.getBitRate();
 
 				 this.params.wifi.radios.push(
@@ -160,7 +161,7 @@ return baseclass.extend({
 						chan : {
 							title: _('Channel'),
 							visible: true,
-							value: chan ? '%d (%.3f %s)'.format(chan, freq, _('GHz')) : '-'
+							value: chan ? '%d (%.3f %s)'.format(chan, freq, frequnit) : '-'
 						},
 
 						rate : {
