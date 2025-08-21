292538f39820d49cb5df55cf2aaccd90ca5c5995
Prabha Murugesan <prabha.murugesan@morsemicro.com>
2025-08-21 01:55:36 +0000

    luci-mod-system: (APP-5196) Option to en-dis-able dropbear instance
    Approved-by: Sophronia Koilpillai
    Approved-by: James Haggerty
    Approved-by: Lyall Beveridge

--- modules/luci-mod-system/htdocs/luci-static/resources/view/system/dropbear.js
+++ modules/luci-mod-system/htdocs/luci-static/resources/view/system/dropbear.js
@@ -14,7 +14,9 @@ return view.extend({
 		s.addremove = true;
 		s.addbtntitle = _('Add instance');
 
-		o = s.option(form.Flag, 'enable', _('Enable Instance'), _('Enable <abbr title="Secure Shell">SSH</abbr> service instance'));
+		o = s.option(form.Flag, 'enable', _('Enable'), _('Enable/Disable this instance'));
+		o.enabled  = '1';
+		o.disabled = '0';
 		o.default  = o.enabled;
 
 		o = s.option(widgets.NetworkSelect, 'Interface', _('Interface'), _('Listen only on the given interface or, if unspecified, on all'));
