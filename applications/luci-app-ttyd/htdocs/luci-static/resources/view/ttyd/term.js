ee2a2104db8b2ec3cc890940a7a4e950e77f4e58
Prabha Murugesan <prabha.murugesan@morsemicro.com>
2025-09-01 03:21:11 +0000

    luci-app-ttyd : (APP-5392, PR #197) Warnings for ttyd with https
    Approved-by: James Haggerty
    Approved-by: Lyall Beveridge

--- applications/luci-app-ttyd/htdocs/luci-static/resources/view/ttyd/term.js
+++ applications/luci-app-ttyd/htdocs/luci-static/resources/view/ttyd/term.js
@@ -4,7 +4,10 @@
 
 return view.extend({
 	load: function() {
-		return uci.load('ttyd');
+		return Promise.all([
+			uci.load('ttyd'),
+			uci.load('uhttpd')
+		]);
 	},
 	render: function() {
 		var port = uci.get_first('ttyd', 'ttyd', 'port') || '7681',
@@ -13,6 +16,36 @@ return view.extend({
 		if (port === '0')
 			return E('div', { class: 'alert-message warning' },
 					_('Random ttyd port (port=0) is not supported.<br />Change to a fixed port and try again.'));
+
+		if (window.location.protocol === 'https:' && ssl === '0') {
+
+			// Check if http is enabled in uhttpd and redirect_https is disabled
+			var http_enabled = (uci.get('uhttpd', 'main', 'listen_http') != null) &&
+							(uci.get('uhttpd', 'main', 'listen_http').length > 0) &&
+							(uci.get('uhttpd', 'main', 'redirect_https') != '1');
+
+			if (http_enabled) {
+				return E('div', { class: 'alert-message warning' }, [
+					E('p', {}, _('Security Warning:')),
+					E('p', {}, _('HTTP is not encrypted. Your password and data can be read on the network.')),
+					E('a', {
+						href: 'http://' + window.location.hostname + L.url('admin/services/ttyd'),
+					}, _('Click to start HTTP session'))
+				]);
+			}
+
+			return E('div', { class: 'alert-message warning' }, [
+				E('p', {}, _('Current terminal configuration is not allowed over HTTPS.')),
+				E('p', {}, [
+					_('Turn off HTTPS redirect in '),
+					E('a', {
+						href: L.url('admin/system/admin/uhttpd'),
+					}, _('HTTP(s) Access')),
+					_(' settings.')
+				])
+			]);
+		}
+
 		return E('iframe', {
 			src: url || ((ssl === '1' ? 'https' : 'http') + '://' + window.location.hostname + ':' + port),
 			style: 'width: 100%; min-height: 500px; border: none; border-radius: 3px; resize: vertical;'
